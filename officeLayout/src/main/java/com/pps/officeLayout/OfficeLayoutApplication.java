fe991173799e5029d37ab8f893f506bcd50a50d0
zeee88 <jihae0808@naver.com>
2024-05-30 10:44:28 +0900

    프로젝트 생성

---
+++ officeLayout/src/main/java/com/pps/officeLayout/OfficeLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.pps.officeLayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeLayoutApplication.class, args);
+	}
+
+}
---
+++ officeLayout/src/test/java/com/pps/officeLayout/OfficeLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pps.officeLayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
